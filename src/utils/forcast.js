1f4fcba12d60a904858f18fb7bc7dffc774e3d99
Faim Tonmoy <bhuiyanfaim@gmail.com>
2021-03-27 11:54:10 +0600

    Add high low

--- src/utils/forcast.js
+++ src/utils/forcast.js
@@ -11,7 +11,7 @@ request({url, json: true}, (error, {body})=>{
 }
   
   else{
-  callback(undefined,body.daily.data[0].summary +" It is currently " +body.currently.temperature +" degree. There is "+ body.currently.precipProbability+" % chance of rain"
+  callback(undefined,body.daily.data[0].summary +" It is currently " +body.currently.temperature +" degree.The High today is "+ body.daily.data[0].temperatureHigh+ " with a low of "+body.daily.data[0].temperatureLow+". There is "+ body.currently.precipProbability+" % chance of rain"
   ) 
 }
 })
